9eefbf499bcb844d4604c2c0b3d3038ac06b3e98
kanziw <kanziwoong@gmail.com>
2021-09-17 13:32:25 +0900

    Separate socket server

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"github.com/sirupsen/logrus"
 	"github.com/slack-go/slack"
-	"github.com/slack-go/slack/slackevents"
 	"github.com/slack-go/slack/socketmode"
 
 	"github.com/kanziw/howareyou/config"
+	"github.com/kanziw/howareyou/server"
 )
 
 func main() {
@@ -23,57 +23,8 @@ func main() {
 		socketmode.OptionDebug(setting.IsDebug),
 	)
 
-	go func() {
-		for evt := range client.Events {
-			log := logrus.StandardLogger().WithFields(logrus.Fields{
-				"evt.type": evt.Type,
-			})
-			switch evt.Type {
-			case socketmode.RequestTypeHello:
-			case socketmode.EventTypeConnecting:
-				log.Debug("Connecting to Slack with Socket Mode...")
-			case socketmode.EventTypeConnectionError:
-				log.Debug("Connection failed. Retrying later...")
-			case socketmode.EventTypeConnected:
-				log.Debug("Connected to Slack with Socket Mode.")
-			case socketmode.EventTypeEventsAPI:
-				eventsAPIEvent, ok := evt.Data.(slackevents.EventsAPIEvent)
-				if !ok {
-					log.WithField("evt", evt).Error("unknown event type:" + evt.Type)
-					continue
-				}
-				client.Ack(*evt.Request)
-
-				log = log.
-					WithFields(logrus.Fields{
-						"evt.data": logrus.Fields{
-							"type": eventsAPIEvent.Type,
-							"inner_event": logrus.Fields{
-								"type": eventsAPIEvent.InnerEvent.Type,
-							},
-						},
-					})
-
-				switch eventsAPIEvent.InnerEvent.Type {
-				case slackevents.ReactionAdded:
-					d, ok := eventsAPIEvent.InnerEvent.Data.(*slackevents.ReactionAddedEvent)
-					if !ok {
-						log.WithField("evt.data.inner_event.data", d).Error("unexpected evt.data.inner_event.data")
-						continue
-					}
-					log.Infof("%s User react using %s on %s's message", d.User, d.Reaction, d.ItemUser)
-				default:
-					log.Info("unsupported Events API event received")
-					client.Debugf("unsupported Events API event received")
-				}
-			// TODO
-			case socketmode.EventTypeInteractive:
-			case socketmode.EventTypeSlashCommand:
-			default:
-				log.Info("unexpected event type received")
-			}
-		}
-	}()
+	s := server.NewSocketServer(client)
+	go s.Listen()
 
 	if err := client.Run(); err != nil {
 		logrus.Fatal(err)
